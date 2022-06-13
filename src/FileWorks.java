4fa40ec7c4b2dd6751190ad03246ee0216f3787b
David Saga <epostsaga@gmail.com>
2022-06-13 02:04:16 +0200

    file reading by alternatives

--- src/FileWorks.java
+++ src/FileWorks.java
@@ -7,41 +7,55 @@ import java.nio.file.*;
 import java.util.List;
 
 public class FileWorks {
-    public static void main(String[] args) throws IOException {
-        Path aFile = FileSystems.getDefault().getPath("", "testFile.txt");
-        List<String> lines = Files.readAllLines(aFile);
-        for (int i = 0; i < lines.size(); i++) {
-            var content = lines.get(i);
-            System.out.println("testFile.txt content : "+content);
-        }
+
+    public static void main(String[] args) {
+
+        readFile("testFile.txt");
+        System.out.println("*********");
+        loadFile("test2.txt");
+
+    }
 
 
-//if try-cath is not used, throws ioexception is added to capsulating mehtod!!!
-//        try {List<String> lines = Files.readAllLines(file);}
-//        catch (IOException e) {throw new RuntimeException(e);}
-
-        // EXAMPLE 4
-// - read a line vs
-//        - read all lines
-
-        var pathname = "testFile.txt";
-        var file = new File(pathname);
-        System.out.println("file exists: " + file.exists());
-
-//invoke in try () will make you sure file is
-// closed after reading. no memory leaks.
-//or else finally and closing needed.
-        try (var reader = new FileReader(file); var buffer = new BufferedReader(reader)) {
-            var oneLine = buffer.readLine();
-            var allLines = Files.readAllLines(Paths.get("fromJShell.java"), Charset.defaultCharset());
-
-            System.out.println("buffer reads oneLine: " + oneLine);
-            System.out.println("ListString reads all: " + allLines);
-
+//  static void readFile(String willRead) throws IOException {
+    static void readFile(String willRead) {
+        Path aFile = FileSystems.getDefault().getPath("", willRead);
+        try {
+            List<String> lines = Files.readAllLines(aFile);
+            //ALTERNATIVE 1 TO READ
+            lines.forEach(str -> System.out.println(str));
         }
         catch (IOException e) {
             throw new RuntimeException(e);
-//            e.printStackTrace();
+        }
+
+// ALTERNATIVE 2 TO READ
+//        for (int i = 0; i < lines.size(); i++) {
+//            var content = lines.get(i);
+//            System.out.println("testFile.txt content : "+content);
+//        }
+
+//ALTERNATIVE 3 TO READ
+//  works in java 11????
+//        lines.forEach(str-> System.out::println);
+
+    }
+
+
+    static void loadFile(String fileName) {
+
+        var file = new File(fileName);
+        System.out.println("file exists: " + file.exists());
+
+        try (var reader = new FileReader(file); var buffer = new BufferedReader(reader)) {
+            var oneLine = buffer.readLine();
+            var allLines = Files.readAllLines(Paths.get(fileName), Charset.defaultCharset());
+
+            System.out.println("buffer reads oneLine: " + oneLine);
+            System.out.println("ListString reads all: " + allLines);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
